07f22e28b7699c626b3d03d696d3d1aa3b23115b
de12161 <162108835+de12161@users.noreply.github.com>
2024-10-26 19:42:58 +0300

    Fix angle method

--- src/main/kotlin/Line.kt
+++ src/main/kotlin/Line.kt
@@ -5,12 +5,11 @@ import kotlin.math.sqrt
 
 internal class Line(private val a: Double, private val b: Double, private val c: Double) {
     private fun angle(l: Line): Double {
-        val cos = abs(a * l.a + b * l.b) / sqrt(a.pow(2) + b.pow(2)) * sqrt(l.a.pow(2) + l.b.pow(2))
-        return acos(cos)
+        return acos(abs(a * l.a + b * l.b) / (sqrt(a.pow(2) + b.pow(2)) * sqrt(l.a.pow(2) + l.b.pow(2))))
     }
     fun intersect(l: Line): Point? {
-        val a = angle(l)
-        if (a == 0.0 || a.isNaN()) return null
+        val ang = angle(l)
+        if (ang == 0.0 || ang.isNaN()) return null
 
         val x = (l.b * c - b * l.c) / (l.a * b - a * l.b)
         val y = (l.a * c - a * l.c) / (a * l.b - l.a * b)
