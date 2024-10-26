eff9a25554df72b42885c2af978e1b409c1ebda3
de12161 <162108835+de12161@users.noreply.github.com>
2024-10-26 19:31:41 +0300

    Fix intersect method

--- src/main/kotlin/Line.kt
+++ src/main/kotlin/Line.kt
@@ -1,15 +1,16 @@
-internal class Line(val a: Double, val b: Double, val c: Double) {
-    private fun isParallel(l: Line): Boolean {
-        if (a == 0.0 && b == 0.0) return false
+import kotlin.math.abs
+import kotlin.math.acos
+import kotlin.math.pow
+import kotlin.math.sqrt
 
-        return (a == l.a && b == l.b) || (a == -l.a && b == -l.b)
+internal class Line(private val a: Double, private val b: Double, private val c: Double) {
+    private fun angle(l: Line): Double {
+        val cos = abs(a * l.a + b * l.b) / sqrt(a.pow(2) + b.pow(2)) * sqrt(l.a.pow(2) + l.b.pow(2))
+        return acos(cos)
     }
     fun intersect(l: Line): Point? {
-        if (
-            isParallel(l)
-            || (a == 0.0 && b == 0.0)
-            || (l.a == 0.0 && l.b == 0.0)
-            ) return null
+        val a = angle(l)
+        if (a == 0.0 || a.isNaN()) return null
 
         val x = (l.b * c - b * l.c) / (l.a * b - a * l.b)
         val y = (l.a * c - a * l.c) / (a * l.b - l.a * b)
