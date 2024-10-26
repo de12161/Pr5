9930c9ed0a4a50f5ceee135f65b8ecd024e5fe51
de12161 <162108835+de12161@users.noreply.github.com>
2024-10-26 15:33:22 +0300

    Fix isParallel method

--- src/main/kotlin/Line.kt
+++ src/main/kotlin/Line.kt
@@ -1,6 +1,6 @@
 internal class Line(val a: Double, val b: Double, val c: Double) {
     private fun isParallel(l: Line): Boolean {
-        if (a == l.a && b == l.b) return true
+        if ((a == l.a && b == l.b) || (a == -l.a && b == -l.b)) return true
         return false
     }
     fun intersect(l: Line): Point? {
